7cfe130bb22b3ee11079c18569d85c011a0aef85
katiej_murphy@outlook.com <katiej_murphy@outlook.com>
2024-03-07 11:56:48 -0500

    updated cache

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import { revalidatePath } from "next/cache";
 import styles from "./page.module.css";
 import { getTime } from '@/util/time';
 
 async function Time() {
   const data = await getTime();
   console.log('data: ' + JSON.parse(data).time);
-
+  revalidatePath('/', 'layout');
   return JSON.parse(data).time;
 }
 
--- util/time.js
+++ util/time.js
@@ -1,6 +1,8 @@
+import { revalidatePath } from "next/cache";
 export async function getTime() {
     const data = JSON.stringify({ time: new Date() });
     console.log('from get time')
     console.log(data)
+    revalidatePath('/', 'layout');
     return data;
 }
